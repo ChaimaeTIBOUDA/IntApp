a254220b654ae45193b703201b31581925ff8b62
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2024-03-26 14:32:13 +0000

    App.js && index.js

---
+++ Front-end/s-app/src/App.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Dashboard from "./Components/Dashboard"
+import Interns from "./Components/Interns"
+import IntDetails from "./Components/IntDetails";
+import Edit from "./Components/Edit";
+import ShowNavbar from "./Components/ShowNavbar";
+import Login from "./Components/Login";
+class App extends Component {
+  
+  render() 
+  {  
+    return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="content">
+          <ShowNavbar>
+          <Header />
+          </ShowNavbar>
+          
+          <Routes>
+            <Route path="/" exact element={<Dashboard />}/>
+            <Route path="/Interns" exact element={<Interns />}/>
+            <Route path="/IntDetails/:id" exact element={<IntDetails />}/>
+            <Route path="/Edit/:id" exact element={<Edit />}/>
+            <Route path="/Login" exact element={<Login />} />
+          </Routes>
+          
+        </div>
+        </BrowserRouter>
+    </div>
+  );
+}
+}
+
+export default App;
