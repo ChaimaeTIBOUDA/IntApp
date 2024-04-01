b3adafc63eb0430923443ada77cb600b032b24b0
Hajarelr <125884338+Hajarelr@users.noreply.github.com>
2024-04-01 22:15:14 +0000

    install jsonwebtoken

--- Back-end/server.js
+++ Back-end/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const multer = require('multer')
+const { v4: uuidv4} = require('uuid')
 require('dotenv').config()
 
 const app = express()
