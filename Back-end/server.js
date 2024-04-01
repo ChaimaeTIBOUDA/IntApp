8f05d79ca66228297d4946c7b8e44352c96acb4e
Hajarelr <125884338+Hajarelr@users.noreply.github.com>
2024-04-01 22:14:42 +0000

    install jsonwebtoken

--- Back-end/server.js
+++ Back-end/server.js
@@ -8,10 +8,11 @@ const app = express()
 const PORT = process.env.PORT | 5000
 const UserModel = require('./models/Users')
 const AdminModel = require('./models/Admin')
+const jwt = require("jsonwebtoken");
 
 app.use(express.json())
 app.use(cors())
-
+app.use(express.static('images'))
 
 mongoose
 .connect(process.env.MONGO_URI)
